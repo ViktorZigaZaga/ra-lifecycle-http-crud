65a7d2972889b12f8d81abe9e9053e3c30e76a28
Viktor Bauer <bassa555@mail.ru>
2023-04-30 14:26:55 +0300

    homework crud for review

---
+++ src/components/NotesList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import Note from './Note';
+
+function NotesList({ notes, onDelete }) {
+  
+    return (
+        <ul className="notes-list">
+            {notes.map((note) => 
+                <li>
+                    <Note key={Date.now() + i*3} note={note} onDelete={onDelete} />
+                </li>
+            )}
+        </ul>
+    )
+}
+
+NotesList.propTypes = {
+    notes: PropTypes.array.isRequired,
+    onDelete: PropTypes.func.isRequired
+}
+
+export default NotesList
